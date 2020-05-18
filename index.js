6240d72a7134e12615029eea236bbb98169d54d4
tunjadomicilios <62493963+tunjadomicilios@users.noreply.github.com>
2020-05-18 16:37:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.querySelector('#enviar').addEventListener('click',function(){
         alert("por favor ingresa todos los campos")
         return null;
     }else{
-          let url = "https://api.whatsapp.com/send?phone=5730541958938&text=*Barberia_Daniel*%0A*Pedidos*%0A%0A*¿Cual es tu nombre?*%0A" + nombre + "%0A*tu telefono:*%0A" + telefono + "%0A*Direccion:*%0A" + direccion + "%0A*Fecha y Hora:*%0A" + fecha +  "%0A*si todo lo anterior es correcto por favor espera nuesra confirmacion*%0A";
+          let url = "https://api.whatsapp.com/send?phone=573054195893&text=*Barberia_Daniel*%0A*Pedidos*%0A%0A*¿Cual es tu nombre?*%0A" + nombre + "%0A*tu telefono:*%0A" + telefono + "%0A*Direccion:*%0A" + direccion + "%0A*Fecha y Hora:*%0A" + fecha +  "%0A*si todo lo anterior es correcto por favor espera nuesra confirmacion*%0A";
     window.open(url);
     }
 
